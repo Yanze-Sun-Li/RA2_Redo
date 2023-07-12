e4bbe8d685462444f733c0fcd547dea259bcbf75
Yanze-Sun-Li <work.yanze.li@gmail.com>
2023-07-12 03:26:54 -0400

    添加搜索附近网格内其他物体的距离的功能，用作自动索敌使用

--- Assets/GridSystem.cs
+++ Assets/GridSystem.cs
@@ -22,11 +22,36 @@ public class Grid
         this.height = height;
         objects = new List<GameObject>();
     }
-
+    public bool IsPositionInGrid(GameObject gameObject)
+    {
+        Vector3 position = gameObject.transform.position;
+        return position.x >= x1 && position.x <= x2 && position.z >= y1 && position.z <= y2;
+    }
     public float calculateArea()
     {
         return width * height;
     }
+    public void AddObject(GameObject obj)
+    {
+        if (!objects.Contains(obj))
+        {
+            objects.Add(obj);
+        }
+    }
+    public void RemoveObject(GameObject obj)
+    {
+        if (objects.Contains(obj))
+        {
+            objects.Remove(obj);
+        }
+    }
+
+    public void MoveObjectToGrid(GameObject obj, Grid targetGrid)
+    {
+        RemoveObject(obj);
+        targetGrid.AddObject(obj);
+    }
+
 }
 
 
@@ -95,6 +120,70 @@ public class GridSystem : MonoBehaviour
         }
     }
 
+    public void AddObjectToGrid(GameObject obj)
+    {
+        foreach (Grid grid in grids)
+        {
+            if (grid.IsPositionInGrid(obj))
+            {
+                // 检查物体是否已经存在于某个网格中
+                if (!grid.objects.Contains(obj))
+                {
+                    grid.MoveObjectToGrid(obj, grid);
+                }
+                break;
+            }
+        }
+    }
+
+    // 获取指定网格周边网格内的物体
+    public List<GameObject> GetNeighboringObjects(Grid grid, int range)
+    {
+        List<GameObject> neighboringObjects = new List<GameObject>();
+
+        // 获取网格的行索引和列索引
+        int rowIndex = Mathf.RoundToInt((grid.y1 - grids[0, 0].y1) / gridHeight);
+        int colIndex = Mathf.RoundToInt((grid.x1 - grids[0, 0].x1) / gridWidth);
+
+        // 计算周边网格的行范围和列范围
+        int startRow = Mathf.Max(0, rowIndex - range);
+        int endRow = Mathf.Min(grids.GetLength(0) - 1, rowIndex + range);
+        int startCol = Mathf.Max(0, colIndex - range);
+        int endCol = Mathf.Min(grids.GetLength(1) - 1, colIndex + range);
+
+        // 遍历周边网格并收集物体
+        for (int row = startRow; row <= endRow; row++)
+        {
+            for (int col = startCol; col <= endCol; col++)
+            {
+                neighboringObjects.AddRange(grids[row, col].objects);
+            }
+        }
+
+        return neighboringObjects;
+    }
+
+    /// <summary>
+    /// 查找在一定的范围之内存在的其他网格物体。（用行和列的位置信息来定位一个网格，）
+    /// </summary>
+    /// <param name="rowIndex">行坐标</param>
+    /// <param name="colIndex">列坐标</param>
+    /// <param name="competObject">指定进行位置信息对比的物体</param>
+    /// <param name="range">搜索圈的范围</param>
+    public void Surrounding(int rowIndex, int colIndex, GameObject competObject, int range) {
+        Grid grid = grids[rowIndex, colIndex];
+         // 设置范围大小
+        List<GameObject> neighboringObjects = GetNeighboringObjects(grid, range);
+
+        foreach (GameObject neighboringObject in neighboringObjects)
+        {
+            // 计算网格内物体与周边网格内物体之间的距离
+            float distance = Vector3.Distance(neighboringObject.transform.position, competObject.transform.position);
+            // 在这里进行处理逻辑
+
+
+        }
+    }
 
     // 在Scene视图中绘制网格系统的边界
     private void OnDrawGizmos()
